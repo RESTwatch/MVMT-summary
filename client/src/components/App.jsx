a872c02299d7cd7d8b1de0feed33e17dee3e09d2
cherrih <cherri.hartigan@gmail.com>
2018-12-21 12:17:51 -0800

    Successfully get data for a specific watch based on wid from client url

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 
-const App = props => {
-  const url = window.location.href;
-  const urlSections = url.split('/');
-  const wid = urlSections[urlSections.length - 2];
-  const displayString = `Watch id on current page is: ${wid}`;
-  return <h1>{displayString}</h1>;
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    const url = window.location.href;
+    const urlSections = url.split('/');
+    const wid = urlSections[urlSections.length - 2];
+    fetch(`/api/watches/${wid}/summary`)
+      .then(response => {
+        return response.json();
+      })
+      .then(myJson => {
+        console.log(JSON.stringify(myJson));
+      })
+  }
   
+  render() {
+    
+    // const displayString = `Watch id on current page is: ${wid}`;
+    return <h1>'Hello'</h1>;
+  }
 }
 
 export default App;
