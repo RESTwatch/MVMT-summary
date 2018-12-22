5984ec81bda9a55c9b8db94b3cb95e664007acea
cherrih <cherri.hartigan@gmail.com>
2018-12-21 17:41:50 -0800

    Fix data fetching of watches without straps

--- database/index.js
+++ database/index.js
@@ -4,8 +4,23 @@ const mysqlConfig = require('./config.js');
 const connection = mysql.createConnection(mysqlConfig);
 
 const getWatchInfo = (watchId, callback) => {
-  const queryString = `SELECT * FROM watches, strap_options, straps WHERE watches.id = strap_options.watch_id AND straps.id = strap_options.strap_id AND watches.wid = ${watchId};`;
-  connection.query(queryString, callback);
+  const queryStringWatchInfo = `SELECT * FROM watches WHERE watches.wid = ${watchId};`;
+  const queryStringStrapInfo = `SELECT * FROM watches, strap_options, straps WHERE watches.id = strap_options.watch_id AND straps.id = strap_options.strap_id AND watches.wid = ${watchId};`;
+  connection.query(queryStringWatchInfo, (watchErr, watchRes) => {
+    if (watchErr) {
+      throw watchErr;
+    } else {
+      const resultArray = [watchRes];
+      connection.query(queryStringStrapInfo, (strapErr, strapRes) => {
+        if (strapErr) {
+          throw strapErr;
+        } else {
+          resultArray.push(strapRes);
+          callback(null, resultArray);
+        }
+      });
+    }
+  });
 };
 
 module.exports = { getWatchInfo };
