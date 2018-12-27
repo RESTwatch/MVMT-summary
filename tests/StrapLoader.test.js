ec1c9bada6d086a802da21a8fb0aeaacceb0afdf
cherrih <cherri.hartigan@gmail.com>
2018-12-27 16:02:17 -0500

    Add a test for StrapLoader component

---
+++ tests/StrapLoader.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import StrapLoader from '../client/src/components/StrapLoader';
+
+describe('StrapLoader component', () => {
+  it('should be defined', () => {
+    expect(StrapLoader).toBeDefined();
+  });
+});
