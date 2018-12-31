f8d955ff24e16d305a623f80fe1700ad8b283bf9
cherrih <cherri.hartigan@gmail.com>
2018-12-31 18:58:49 -0500

    Successfully style strap guide modal

--- client/src/components/StrapGuide.jsx
+++ client/src/components/StrapGuide.jsx
@@ -23,17 +23,23 @@ class StrapGuide extends React.Component {
         <img id="summary-question-mark" src="https://s3.amazonaws.com/watch-straps/question-mark.png"/>
         <span onClick={this.onOpenModal.bind(this)}>STRAP GUIDE</span>
         <Modal open={this.state.open} onClose={this.onCloseModal.bind(this)} center>
+          <div className="summary-strap-guide-modal">
           <h2>INTERCHANGEABLE STRAPS GUIDE</h2>
-          <p>
+          <p className="summary-strap-guide-info">
             Modern Sport watches feature a quick-release clasp that allows you to easily swap out your strap at home…no tools required.
           </p>
-          <div>
+          <div className="summary-strap-guide-images">
             <span>
-              
+              <img src="https://s3.amazonaws.com/watch-straps/remove.jpg"/>
+              <div className="summary-strap-step-title">REMOVE</div>
+              <p className="summary-strap-step-instructions">Using your thumb, slide the lever sideways and remove from holes.</p>
             </span>
             <span>
-
+              <img src="https://s3.amazonaws.com/watch-straps/attach.jpg"/>
+              <div className="summary-strap-step-title">ATTACH</div>
+              <p className="summary-strap-step-instructions">With the lever held back, place strap into holes and release to secure.</p>
             </span>
+          </div>
           </div>
         </Modal>
       </span>
